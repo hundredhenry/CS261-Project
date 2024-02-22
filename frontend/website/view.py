27ce5f62b8dd8c53e0d656bf13d379bed03f2856
Jheels Patel <patej162.316@gmail.com>
2024-02-22 11:09:05 +0000

    Added length checks to prevent html form check being bypassed

--- frontend/website/view.py
+++ frontend/website/view.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user
 
 from . import db
 from .models import User
@@ -23,28 +24,32 @@ def register():
         confirm_password = request.form.get('confirm_password')
         
         # check if the user is already registered 
-        user_exists = User.query.filter_by(email=email).first()
-        
-        if user_exists:
-            flash("This email is already registered!", category="error")
-            return redirect(url_for("views.test_page"))
-        # if not, continue registering the user
-        
+        if len(name) < 1:
+            flash("Name is too short!", category="name_error")
+            return render_template('register.html')
+        if len(email) < 1:
+            flash("Email is too short!", category="email_error")
+            return render_template('register.html')
+                
         if not re.match("^[a-zA-Z]*$", name):
-            flash('Name must contain only letters', category='error')
-        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
-            flash('Invalid email address', category='error')
+            flash('Name must contain only letters', category='name_error')
+        elif not re.match(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-]{2,}$)", email):
+            flash('Invalid email address', category='email_error')
         elif password != confirm_password:
-            flash('Passwords do not match', category='error')
+            flash('Passwords do not match', category='pw_error')
         elif len(password) < 8:
-            flash('Password must be at least 8 characters', category='error')
+            flash('Password must be at least 8 characters', category='pw_error')
         else:
+            user_exists  = User.query.filter_by(email=email).first()
+            if user_exists:
+                flash("This email is already registered!", category="email_error")
+                return redirect(url_for("views.test_page"))
+
             new_user = User(firstname=name, email=email, password_hash=generate_password_hash(password))# create a new user with generate_password_hash(password) default values are sufficient
             # add this user to the DB
             db.session.add(new_user)
             db.session.commit()
             # begin user verification via email check
-            flash("User has been registered, please activate your account", category="success")
             # do email sending process here
             token = generate_confirmation_token(email)
             confirm_url = url_for('views.confirm_email', token=token, _external=True)
@@ -53,8 +58,6 @@ def register():
             
             send_email(subject, email, html)
             
-            flash('A confirmation email has been sent via email.', 'success')
-
             return render_template('unconfirmed.html')
 
     return render_template('register.html')
@@ -89,17 +92,15 @@ def login():
         
         if user and not user.verified:
             flash("Please verify your email first", category="error")
-            print("Please verify your email first")
             return redirect(url_for("views.test_page"))
         
         if user and check_password_hash(user.password_hash, password):
             flash("Logged in successfully", category="success")
-            print("Logged in successfully")
+            login_user(user, remember=True)
             return redirect(url_for("views.test_page"))
         else:
             flash("Email or password is incorrect", category="error")
-            print("Email or password is incorrect")
     return render_template('login.html')
     
-    
+
 
