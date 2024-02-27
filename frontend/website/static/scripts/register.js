022827fdef090b552973972c6c03e137d22337e8
Jheels Patel <patej162.316@gmail.com>
2024-02-27 16:19:56 +0000

    Overhauled registration page and minor testing fix

---
+++ frontend/website/static/scripts/register.js
@@ -0,0 +1,11 @@
+function togglePasswordVisibility() {
+    var password = document.getElementsByName('password')[0];
+    var confirm_password = document.getElementsByName('confirm_password')[0];
+    if (password.type === 'password') {
+        password.type = 'text';
+        confirm_password.type = 'text';
+    } else {
+        password.type = 'password';
+        confirm_password.type = 'password';
+    }
+}
