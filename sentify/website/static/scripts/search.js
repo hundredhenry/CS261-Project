cc17432cce62b6d4f2243ae227cf14f8999fac7e
Jheels Patel <patej162.316@gmail.com>
2024-02-29 14:35:58 +0000

    Overall search bar improvements

--- sentify/website/static/scripts/search.js
+++ sentify/website/static/scripts/search.js
@@ -6,38 +6,28 @@ window.onload = function() {
         .then(data => companies = data);
 }
 
-function searchFunction(query) {
+function searchCompany(query) {
     let results = query ? companies.filter(company => company.company_name.toLowerCase().includes(query.toLowerCase()) || company.stock_ticker.toLowerCase().includes(query.toLowerCase())) : [];
     results = results.slice(0, 5); // limit the results to a maximum of 5
 
     const resultsDiv = document.getElementById('results');
     resultsDiv.innerHTML = ''; // clear previous results
+
     if (results.length === 0) {  
         if (query) {
-            resultsDiv.textContent = 'No matches';
+            resultsDiv.innerHTML = `<div class="result-item"><span>No Matches...</span></div>`;
         } else {
             resultsDiv.style.display = 'none';
         }
     } else {
         resultsDiv.style.display = 'block';
-        results.forEach(company => {
-            const a = document.createElement('a');
-            a.href = '/company/' + company.stock_ticker;
-
-            const div = document.createElement('div');
-            div.className = 'result-item'; // apply the CSS class
-
-            const nameSpan = document.createElement('span');
-            nameSpan.textContent = company.company_name;
-            div.appendChild(nameSpan);
-
-            const tickerSpan = document.createElement('span');
-            tickerSpan.textContent = company.stock_ticker;
-            tickerSpan.className = 'ticker'; // apply a different CSS class
-            div.appendChild(tickerSpan);
-
-            a.appendChild(div);
-            resultsDiv.appendChild(a);
-        });
+        resultsDiv.innerHTML = results.map(company => `
+            <a href="/company/${company.stock_ticker}">
+                <div class="result-item">
+                    <span>${company.company_name}</span>
+                    <span class="ticker">${company.stock_ticker}</span>
+                </div>
+            </a>
+        `).join('');
     }
 }
